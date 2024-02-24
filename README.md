ad362ecedac5f267d1d498a8fa00189492a70bb7
mastitesfa444@gmail.com <kidistkifle31@gmail.com>
2024-02-24 12:20:57 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# responsive_quotes-generator" 
---
+++ index.js
@@ -0,0 +1,17 @@
+
+const apiurl = "https://api.quotable.io/random";
+
+async function getJock(){
+    const response = await fetch(apiurl);
+    var data = await response.json();
+
+    const Quotes = data.content;
+    const author = data.author;
+
+    document.querySelector(".middle p").innerHTML = Quotes;
+    document.querySelector(".author p").innerHTML = "---"+author;
+}
+
+document.querySelector("button").addEventListener("click", function(){
+    getJock();
+})
